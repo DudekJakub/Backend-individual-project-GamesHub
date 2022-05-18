52ade699f31c1e1090d9d2597d14608c1aea9b33
jakub <jakub.dudek94@gmail.com>
2022-05-18 18:37:09 +0200

    Few fixes in RegistrationController added

--- src/main/java/com/gameshub/controller/RegistrationController.java
+++ src/main/java/com/gameshub/controller/RegistrationController.java
@@ -1,32 +1,33 @@
 package com.gameshub.controller;
 
 import com.gameshub.domain.user.RegistrationRequestDto;
-import com.gameshub.exceptions.*;
+import com.gameshub.exception.*;
 import com.gameshub.service.RegistrationService;
 import com.gameshub.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/registration/")
+@RequestMapping("/v1/registrations")
 public class RegistrationController {
 
     private final RegistrationService registrationService;
     private final UserService userService;
 
-    @PostMapping(value = "register")
-    public ResponseEntity<String> register(@RequestBody RegistrationRequestDto request) throws UserEmailAlreadyExistsInDatabaseException,
-                                                                                               UserLoginNameAlreadyExistsInDatabaseException,
-                                                                                               PasswordNotMatchException {
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> register(@RequestBody RegistrationRequestDto request) throws UserEmailAlreadyExistsInDatabaseException, UserLoginNameAlreadyExistsInDatabaseException,
+                                                                                               PasswordNotMatchException, UserNotFoundException {
         return ResponseEntity.ok(registrationService.register(request));
     }
 
     @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
-    @GetMapping(value = "confirm/{loginName}")
-    public ResponseEntity<String> confirm(@PathVariable String loginName) throws AccessDeniedException, UserNotFoundException, UserAlreadyVerifiedException {
+    @GetMapping("/confirm/{loginName}")
+    public ResponseEntity<String> confirm(@PathVariable String loginName) throws AccessDeniedException, UserNotFoundException,
+                                                                                 UserAlreadyVerifiedException, UserNotVerifiedException {
         return ResponseEntity.ok(registrationService.confirmAccount(userService.getUserByLoginName(loginName)));
     }
 }
