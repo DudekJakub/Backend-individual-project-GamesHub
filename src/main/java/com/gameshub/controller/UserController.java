4be914d115609740031316446fad98b82a8f7265
jakub <jakub.dudek94@gmail.com>
2022-05-18 18:37:24 +0200

    Few fixes in UserController added

--- src/main/java/com/gameshub/controller/UserController.java
+++ src/main/java/com/gameshub/controller/UserController.java
@@ -2,35 +2,41 @@ package com.gameshub.controller;
 
 import com.gameshub.domain.user.UserCloseDto;
 import com.gameshub.domain.user.UserOpenDto;
+import com.gameshub.exception.UserIsNotAdminException;
+import com.gameshub.exception.UserNotFoundException;
+import com.gameshub.exception.UserNotVerifiedException;
+import com.gameshub.facade.UserFacade;
 import com.gameshub.mapper.user.UserMapper;
 import com.gameshub.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/")
+@RequestMapping("/v1/users")
 public class UserController {
 
-    private final UserService userService;
-    private final UserMapper userMapper;
+    private final UserFacade userFacade;
 
     @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
-    @GetMapping(value = "users")
-    public ResponseEntity<List<UserOpenDto>> users() {
-        return ResponseEntity.ok(userMapper.mapToUserOpenDtoList(userService.getAllUsers()));
+    @GetMapping()
+    public ResponseEntity<List<UserOpenDto>> users() throws UserNotVerifiedException {
+        return ResponseEntity.ok(userFacade.fetchUsersListForRegularUser());
     }
 
     @PreAuthorize("hasAuthority('ADMIN')")
-    @GetMapping(value = "admin/users")
-    public ResponseEntity<List<UserCloseDto>> usersForAdmin() {
-        return ResponseEntity.ok(userMapper.mapToUserCloseDtoList(userService.getAllUsers()));
+    @GetMapping("/admin")
+    public ResponseEntity<List<UserCloseDto>> usersForAdmin() throws UserNotFoundException, UserIsNotAdminException {
+        return ResponseEntity.ok(userFacade.fetchUsersListForAdmin());
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    @PutMapping("/setNotificationStrategy/{userId}")
+    public ResponseEntity<String> updateUserNotificationStrategy(@PathVariable Long userId, @RequestParam String notificationStrategy) throws UserNotFoundException, UserNotVerifiedException {
+        return ResponseEntity.ok(userFacade.setUserNotificationStrategy(userId, notificationStrategy));
     }
 }
