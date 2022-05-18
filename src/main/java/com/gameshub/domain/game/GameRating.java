04ebfb792917c8a085fae9869d7382c063198226
jakub <jakub.dudek94@gmail.com>
2022-05-18 18:34:22 +0200

    Changes in entity_relationships in GameRating added (this is still 'to check')

--- src/main/java/com/gameshub/domain/game/GameRating.java
+++ src/main/java/com/gameshub/domain/game/GameRating.java
@@ -4,11 +4,11 @@ import com.gameshub.domain.user.User;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import com.sun.istack.NotNull;
 import java.time.LocalDateTime;
 
 @Data
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,45 +17,24 @@ public class GameRating {
 
     @Id
     @EqualsAndHashCode.Include
-    @NotNull
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID")
     private Long id;
 
     @NotNull
-    @EqualsAndHashCode.Exclude
-    @Column(name = "GAME_NAME")
     private String gameName;
 
     @NotNull
-    @EqualsAndHashCode.Exclude
-    @Column(name = "RATING")
     private int rating;
 
     @NotNull
-    @EqualsAndHashCode.Exclude
-    @Column(name = "PUBLICATION_DATE")
     private final LocalDateTime publicationDate = LocalDateTime.now();
 
-
-    @EqualsAndHashCode.Exclude
-    @ManyToOne(cascade = {
-            CascadeType.DETACH,
-            CascadeType.MERGE,
-            CascadeType.PERSIST,
-            CascadeType.REFRESH
-    }, fetch = FetchType.EAGER)
+    @ManyToOne
     @JoinColumn(name = "GAME_ID")
     private Game game;
 
-
-    @EqualsAndHashCode.Exclude
-    @ManyToOne(cascade = {
-            CascadeType.DETACH,
-            CascadeType.MERGE,
-            CascadeType.PERSIST,
-            CascadeType.REFRESH
-    }, fetch = FetchType.EAGER)
+    @ManyToOne
     @JoinColumn(name = "USER_ID")
     private User user;
 }
