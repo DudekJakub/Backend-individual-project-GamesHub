17d51e3416d9c8721f22bbf5ab4b754b41982811
jakub <jakub.dudek94@gmail.com>
2022-05-18 18:32:44 +0200

    Entity_relationships in GameOpinion updated

--- src/main/java/com/gameshub/domain/game/GameOpinion.java
+++ src/main/java/com/gameshub/domain/game/GameOpinion.java
@@ -3,15 +3,15 @@ package com.gameshub.domain.game;
 import com.gameshub.domain.user.User;
 import com.sun.istack.NotNull;
 import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NamedEntityGraph(
         name = "graph.GameOpinion.user",
-        attributeNodes = @NamedAttributeNode("user")
-)
+        attributeNodes = @NamedAttributeNode("user"))
 @Data
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,65 +20,28 @@ public class GameOpinion {
 
     @Id
     @EqualsAndHashCode.Include
-    @NotNull
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID")
     private Long id;
 
     @NotNull
-    @EqualsAndHashCode.Exclude
-    @Column(name = "GAME_NAME")
     private String gameName;
 
     @NotNull
-    @EqualsAndHashCode.Exclude
-    @Column(name = "USER_LOGIN")
+    @EqualsAndHashCode.Include
     private String userLogin;
 
     @NotNull
-    @EqualsAndHashCode.Exclude
-    @Column(name = "OPINION")
     private String opinion;
 
     @NotNull
-    @EqualsAndHashCode.Exclude
-    @Column(name = "PUBLICATION_DATE")
     private final LocalDateTime publicationDate = LocalDateTime.now();
 
-
-    @EqualsAndHashCode.Exclude
-    @ManyToOne(cascade = {
-            CascadeType.DETACH,
-            CascadeType.MERGE,
-            CascadeType.PERSIST,
-            CascadeType.REFRESH
-    }, fetch = FetchType.LAZY)
+    @ManyToOne(targetEntity = Game.class, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "GAME_ID")
     private Game game;
 
-
-    @EqualsAndHashCode.Exclude
-    @ManyToOne(cascade = {
-            CascadeType.DETACH,
-            CascadeType.MERGE,
-            CascadeType.PERSIST,
-            CascadeType.REFRESH
-    }, fetch = FetchType.LAZY)
+    @ManyToOne(targetEntity = User.class, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "USER_ID")
     private User user;
-
-
-
-    @Override
-    public String toString() {
-        return "\nGameOpinion{" +
-                "\nid=" + id +
-                "\ngameName='" + gameName + '\'' +
-                "\nuserLogin='" + userLogin + '\'' +
-                "\nopinion='" + opinion + '\'' +
-                "\npublicationDate=" + publicationDate +
-                "\ngame=" + game +
-                "\nuser=" + user +
-                '}';
-    }
 }
