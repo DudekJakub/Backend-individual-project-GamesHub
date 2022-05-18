0a68d1e7443288d86791820c4f2828e7d419292c
jakub <jakub.dudek94@gmail.com>
2022-05-18 18:36:54 +0200

    Few fixes in RawgGameController added

--- src/main/java/com/gameshub/controller/RawgGameController.java
+++ src/main/java/com/gameshub/controller/RawgGameController.java
@@ -1,30 +1,27 @@
 package com.gameshub.controller;
 
-import com.gameshub.domain.game.rawgGame.detailed.RawgGameDetailedDto;
+import com.gameshub.domain.game.rawgGame.RawgGameDetailedDto;
 import com.gameshub.service.RawgService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/games/")
+@RequestMapping("/v1/rawg_games")
 public class RawgGameController {
 
     private final RawgService rawgService;
 
-    @GetMapping(value = "{name}")
-    public ResponseEntity<List<RawgGameDetailedDto>> rawgGamesRelatedToGivenName(@PathVariable String name) {
-        return ResponseEntity.ok(rawgService.fetchListOfRawgGamesRelatedToGivenName(name));
-    }
-
-    @GetMapping(value = "/id/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<RawgGameDetailedDto> rawgGameById(@PathVariable Long id) {
         return ResponseEntity.ok(rawgService.getRawgGameDetailedById(id));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<RawgGameDetailedDto>> rawgGamesRelatedToGivenName(@RequestParam String name) {
+        return ResponseEntity.ok(rawgService.fetchListOfRawgGamesRelatedToGivenName(name));
     }
 }
