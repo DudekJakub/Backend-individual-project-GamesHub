d8d97b27f4e8fbc3d2788f1b4128af5f227b1e57
jakub <jakub.dudek94@gmail.com>
2022-05-18 18:31:30 +0200

    New entity_relationships + some fixes in Game_entity added

--- src/main/java/com/gameshub/domain/game/Game.java
+++ src/main/java/com/gameshub/domain/game/Game.java
@@ -1,11 +1,14 @@
 package com.gameshub.domain.game;
 
+import com.gameshub.domain.user.User;
 import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @NamedNativeQuery(
         name = "Game.retrieveGamesWhereNameIsLike",
@@ -14,7 +17,7 @@ import java.util.List;
         name = "graph.Game.gameOpinions",
         attributeNodes = @NamedAttributeNode("gameOpinions"))
 @Data
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,18 +26,25 @@ public class Game {
 
     @Id
     @EqualsAndHashCode.Include
+    @NotNull
     @Column(name = "ID", unique = true)
     private Long id;
 
     @NotNull
-    @EqualsAndHashCode.Exclude
-    @Column(name = "NAME")
     private String name;
 
-    @EqualsAndHashCode.Exclude
-    @OneToMany(targetEntity = GameOpinion.class,
-               mappedBy = "game",
-               cascade = CascadeType.PERSIST,
-               fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "game")
     private final List<GameOpinion> gameOpinions = new ArrayList<>();
+
+    @ManyToMany(targetEntity = User.class, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(name = "GAMES_HAVE_OBSERVERS",
+               joinColumns =        {@JoinColumn(name = "GAME_ID", referencedColumnName = "ID")},
+               inverseJoinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")})
+    private final Set<User> observers = new HashSet<>();
+
+    @ManyToMany(mappedBy = "gamesOwned")
+    private final Set<User> usersOwnedThisGame = new HashSet<>();
+
+    @ManyToMany(mappedBy = "gamesWantedToOwn")
+    private final Set<User> usersWantedToOwnThisGame = new HashSet<>();
 }
