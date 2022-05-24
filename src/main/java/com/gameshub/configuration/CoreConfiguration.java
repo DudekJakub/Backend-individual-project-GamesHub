31461e7e55b2ff866bd0ad29fa75ebf832231a5f
jakub <jakub.dudek94@gmail.com>
2022-05-24 23:13:50 +0200

    CoreConfiguration class #UPDATE

--- src/main/java/com/gameshub/configuration/CoreConfiguration.java
+++ src/main/java/com/gameshub/configuration/CoreConfiguration.java
@@ -1,7 +1,12 @@
 package com.gameshub.configuration;
 
+import com.gameshub.domain.user.User;
+import com.gameshub.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
@@ -10,9 +15,15 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.List;
+
 @Configuration
+@EnableScheduling
 @EnableSwagger2
+@RequiredArgsConstructor
 public class CoreConfiguration {
+
+    private final UserRepository userRepository;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -31,5 +42,17 @@ public class CoreConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("com.gameshub"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    @Scope(value = "prototype")
+    public List<User> confirmedUsers() {
+        return userRepository.retrieveVerifiedUsers();
+    }
+
+    @Bean
+    @Scope(value = "prototype")
+    public List<User> admins() {
+        return userRepository.retrieveAdmins();
     }
 }
