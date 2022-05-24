b7e14939399cc0e12d652855b56a12355c53de2e
jakub <jakub.dudek94@gmail.com>
2022-05-24 23:26:29 +0200

    NotificationStrategy interface #UPDATE

--- src/main/java/com/gameshub/subscribe/strategy/NotificationStrategy.java
+++ src/main/java/com/gameshub/subscribe/strategy/NotificationStrategy.java
@@ -1,9 +1,10 @@
 package com.gameshub.subscribe.strategy;
 
 import com.gameshub.subscribe.GameOpinionAddedEvent;
+import com.gameshub.subscribe.GameRatingAddedEvent;
 
 public interface NotificationStrategy {
 
     void sendGameOpinionNotification(GameOpinionAddedEvent gameOpinionAddedEvent);
-    void sendGameRatingNotification();
+    void sendGameRatingNotification(GameRatingAddedEvent gameRatingAddedEvent);
 }
