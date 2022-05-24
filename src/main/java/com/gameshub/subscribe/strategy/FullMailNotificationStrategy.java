4d01dfe683c892fc90ec3d7609ad90e462d790ed
jakub <jakub.dudek94@gmail.com>
2022-05-24 23:26:11 +0200

    Full & Simple NotificationStrategy updated by adding 'sendGameRatingNotification' #UPDATE

--- src/main/java/com/gameshub/subscribe/strategy/FullMailNotificationStrategy.java
+++ src/main/java/com/gameshub/subscribe/strategy/FullMailNotificationStrategy.java
@@ -1,23 +1,131 @@
 package com.gameshub.subscribe.strategy;
 
-import com.gameshub.email.EmailService;
+import com.gameshub.domain.game.GameOpinion;
+import com.gameshub.domain.game.GameRating;
+import com.gameshub.domain.mail.Mail;
+import com.gameshub.domain.user.User;
+import com.gameshub.email.EmailNotificationService;
+import com.gameshub.email.builder.EmailBuilder;
+import com.gameshub.repository.GameOpinionRepository;
+import com.gameshub.repository.GameRatingRepository;
 import com.gameshub.subscribe.GameOpinionAddedEvent;
+import com.gameshub.subscribe.GameRatingAddedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class FullMailNotificationStrategy implements NotificationStrategy {
 
-    private final EmailService emailService;
+    private final EmailNotificationService emailNotificationService;
+    private final GameOpinionRepository gameOpinionRepository;
+    private final GameRatingRepository gameRatingRepository;
 
     @Override
-    public void sendGameOpinionNotification(GameOpinionAddedEvent gameOpinionAddedEvent) {
+    public void sendGameOpinionNotification(final GameOpinionAddedEvent oEvent) {
+        List<List<String>> gameAndOpinionInformation = new ArrayList<>();
+        List<GameOpinion> fourLatestGameOpinions = gameOpinionRepository.retrieveThreeLatestGameOpinionsForGame(oEvent.getGameId());
+        List<String> dataForEmail = prepareDataFromLatestGameOpinions(fourLatestGameOpinions);
 
+        List<String> opinionInformation = new ArrayList<>();
+        opinionInformation.add("NEW OPINION from [" + oEvent.getUserName() + "]: " + oEvent.getNewOpinion());
+        opinionInformation.add("at " + oEvent.getPubDate());
+        opinionInformation.add("LATEST OPINIONS: ");
+        opinionInformation.addAll(dataForEmail);
+
+        gameAndOpinionInformation.add(prepareGameInformationList(oEvent.getGameName(),  oEvent.getOpinionsQnt(), oEvent.getRatingsQnt(),
+                                                                 oEvent.getOpinionsPerDay(), oEvent.getRatingsPerDay(), oEvent.getAverageCurrentRating()));
+        gameAndOpinionInformation.add(opinionInformation);
+
+        Mail mail = EmailBuilder.createMail(
+                oEvent.getUserEmail(),
+                "GamesHub - new opinion notification",
+                "There is new opinion for game You have subscribed!"
+        );
+        emailNotificationService.sendGameOpinionNotifyingMail(mail, oEvent.getUserName(), oEvent.getGameOpinion(), gameAndOpinionInformation);
     }
 
     @Override
-    public void sendGameRatingNotification() {
+    public void sendGameRatingNotification(final GameRatingAddedEvent rEvent) {
+        List<List<String>> gameAndRatingInformation = new ArrayList<>();
+        List<GameRating> sixLatestGameRatings = gameRatingRepository.retrieveSixLatestGameRatingsForGame(rEvent.getGameId());
+        List<String> dataForEmail = prepareDataFromLatestGameRatings(sixLatestGameRatings);
 
+        List<String> ratingInformation = new ArrayList<>();
+        ratingInformation.add("NEW RATING from [" + rEvent.getUserName() + "]: " + rEvent.getNewRating() + " at " + rEvent.getPubDate());
+        ratingInformation.add("LATEST RATINGS: ");
+        ratingInformation.addAll(dataForEmail);
+
+        gameAndRatingInformation.add(prepareGameInformationList(rEvent.getGameName(), rEvent.getOpinionsQnt(), rEvent.getRatingsQnt(),
+                                                                rEvent.getOpinionsPerDay(), rEvent.getRatingsPerDay(), rEvent.getAverageCurrentRating()));
+        gameAndRatingInformation.add(ratingInformation);
+
+        Mail mail = EmailBuilder.createMail(
+                rEvent.getUserEmail(),
+                "GamesHub - new rating notification",
+                "There are new ratings for game You have subscribed!"
+        );
+        emailNotificationService.sendGameRatingNotifyingMail(mail, rEvent.getUserName(), rEvent.getGameRating(), gameAndRatingInformation);
+    }
+
+    private List<String> prepareGameInformationList(final String gameName, final int opinionsQnt, final int ratingsQnt,
+                                                    final double opinionsPerDay, final double ratingsPerDay, final double avgCurrRating) {
+        List<String> gameInformation = new ArrayList<>();
+        gameInformation.add("GAME NAME: " + gameName);
+        gameInformation.add("OPINIONS QUANTITY: " + opinionsQnt);
+        gameInformation.add("RATINGS QUANTITY: " + ratingsQnt);
+        gameInformation.add("AVG. OPINIONS PER DAY: " + opinionsPerDay);
+        gameInformation.add("AVG. RATINGS PER DAY: " + ratingsPerDay);
+        gameInformation.add("AVG. CURRENT RATING: " + avgCurrRating);
+
+        return gameInformation;
+    }
+
+    private List<String> prepareDataFromLatestGameOpinions(final List<GameOpinion> fourLatestGameOpinions) {
+        String userFirstname;
+        String userOpinion;
+        LocalDateTime userOpinionPubDate;
+        User userWithOpinion;
+
+        fourLatestGameOpinions.remove(0);
+
+        List<String> resultList = new ArrayList<>();
+
+        for (GameOpinion opinion : fourLatestGameOpinions) {
+            userWithOpinion = opinion.getUser();
+            userFirstname = userWithOpinion.getFirstname();
+            userOpinion = opinion.getOpinion();
+            userOpinionPubDate = opinion.getPublicationDate();
+
+            resultList.add("[" + userFirstname + "] wrote opinion:");
+            resultList.add("\"" + userOpinion + "\"");
+            resultList.add("[PUB. DATE] " + userOpinionPubDate);
+        }
+        return resultList;
+    }
+
+    private List<String> prepareDataFromLatestGameRatings(final List<GameRating> sixLatestGameRatings) {
+        String userFirstname;
+        double userRating;
+        LocalDateTime userRatingPubDate;
+        User userWithRating;
+
+        sixLatestGameRatings.remove(0);
+
+        List<String> resultList = new ArrayList<>();
+
+        for (GameRating rating : sixLatestGameRatings) {
+            userWithRating = rating.getUser();
+            userFirstname = userWithRating.getFirstname();
+            userRating = rating.getRating();
+            userRatingPubDate = rating.getPublicationDate();
+
+            resultList.add("[" + userFirstname + "] added rating: " + userRating + " at " + userRatingPubDate);
+        }
+        return resultList;
     }
 }
--- src/main/java/com/gameshub/subscribe/strategy/SimpleEmailNotificationStrategy.java
+++ src/main/java/com/gameshub/subscribe/strategy/SimpleEmailNotificationStrategy.java
@@ -1,37 +1,66 @@
 package com.gameshub.subscribe.strategy;
 
-import com.gameshub.domain.game.GameOpinion;
 import com.gameshub.domain.mail.Mail;
-import com.gameshub.email.EmailBuilder;
-import com.gameshub.email.EmailService;
+import com.gameshub.email.EmailNotificationService;
+import com.gameshub.email.builder.EmailBuilder;
 import com.gameshub.subscribe.GameOpinionAddedEvent;
+import com.gameshub.subscribe.GameRatingAddedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class SimpleEmailNotificationStrategy implements NotificationStrategy {
 
-    private final EmailService emailService;
+    private final EmailNotificationService emailNotificationService;
 
     @Override
-    public void sendGameOpinionNotification(GameOpinionAddedEvent gameOpinionAddedEvent) {
-        String userName = gameOpinionAddedEvent.getUserName();
-        String newOpinion = gameOpinionAddedEvent.getNewOpinion();
-        String userEmail = gameOpinionAddedEvent.getUserEmail();
-        GameOpinion gameOpinion = gameOpinionAddedEvent.getGameOpinion();
-        int opinionsQnt = gameOpinionAddedEvent.getOpinionsQnt();
+    public void sendGameOpinionNotification(final GameOpinionAddedEvent oEvent) {
+        List<List<String>> gameAndOpinionInformation = new ArrayList<>();
+        List<String> opinionInformation = new ArrayList<>();
+        List<String> gameInformation = prepareGameInformationList(oEvent.getGameName(), oEvent.getOpinionsQnt(), oEvent.getRatingsQnt(), oEvent.getAverageCurrentRating());
+
+        opinionInformation.add("NEW OPINION from [" +  oEvent.getUserName() + "]: " + oEvent.getNewOpinion());
+
+        gameAndOpinionInformation.add(gameInformation);
+        gameAndOpinionInformation.add(opinionInformation);
 
         Mail mail = EmailBuilder.createMail(
-                userEmail,
+                oEvent.getUserEmail(),
                 "GamesHub - new opinion notification",
                 "There is new opinion for game You have subscribed!"
         );
-        emailService.sendGameOpinionNotifyingMail(mail, userName, gameOpinion, opinionsQnt, newOpinion);
+        emailNotificationService.sendGameOpinionNotifyingMail(mail, oEvent.getUserName(), oEvent.getGameOpinion(), gameAndOpinionInformation);
     }
 
     @Override
-    public void sendGameRatingNotification() {
+    public void sendGameRatingNotification(final GameRatingAddedEvent rEvent) {
+        List<List<String>> gameAndRatingInformation = new ArrayList<>();
+        List<String> ratingInformation = new ArrayList<>();
+        List<String> gameInformation = prepareGameInformationList(rEvent.getGameName(), rEvent.getOpinionsQnt(), rEvent.getRatingsQnt(), rEvent.getAverageCurrentRating());
 
+        ratingInformation.add("NEW RATING from [" + rEvent.getUserName() + "]: " + rEvent.getNewRating() + " at " + rEvent.getPubDate());
+
+        gameAndRatingInformation.add(gameInformation);
+        gameAndRatingInformation.add(ratingInformation);
+
+        Mail mail = EmailBuilder.createMail(
+                rEvent.getUserEmail(),
+                "GamesHub - new rating notification",
+                "There are new ratings for game You have subscribed!"
+        );
+        emailNotificationService.sendGameRatingNotifyingMail(mail, rEvent.getUserName(), rEvent.getGameRating(), gameAndRatingInformation);
+    }
+
+    private List<String> prepareGameInformationList(final String gameName, final int opinionsQnt, final int ratingsQnt, final double avgCurrRating) {
+        List<String> gameInformation = new ArrayList<>();
+        gameInformation.add("GAME NAME: " + gameName);
+        gameInformation.add("OPINIONS QUANTITY: " + opinionsQnt);
+        gameInformation.add("RATINGS QUANTITY: " + ratingsQnt);
+
+        return gameInformation;
     }
 }
