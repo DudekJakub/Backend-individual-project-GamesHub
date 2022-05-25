d147fc914816f958b07adeeb666b5f9f094c3bc9
jakub <jakub.dudek94@gmail.com>
2022-05-25 23:21:07 +0200

    RawgClientTest moved from... #TEST/REFACTOR

--- src/test/java/com/gameshub/client/config/RawgApiConfigTest.java
+++ src/test/java/com/gameshub/client/config/RawgApiConfigTest.java
@@ -1,4 +1,4 @@
-package com.gameshub.rawg.config;
+package com.gameshub.client.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class RawgConfigTest {
+class RawgApiConfigTest {
 
     @Autowired
-    private RawgConfig rawgConfig;
+    private RawgApiConfig rawgApiConfig;
 
     @Test
     void getRawgApiEndpoint() {
         //Given & When
-        var rawgApiEndpoint = rawgConfig.getRawgApiEndpoint();
+        var rawgApiEndpoint = rawgApiConfig.getRawgApiEndpoint();
 
         //Then
         assertEquals("https://api.rawg.io/api/", rawgApiEndpoint);
@@ -24,7 +24,7 @@ class RawgConfigTest {
     @Test
     void getRawgAppKey() {
         //Given & When
-        var rawgAppKey = rawgConfig.getRawgAppKey();
+        var rawgAppKey = rawgApiConfig.getRawgAppKey();
 
         //Then
         assertEquals("7619ad3c89dd42838b84961b780df5eb", rawgAppKey);
